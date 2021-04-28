8862ec0f22fb60984415e543818b48bdddbe7906
tara <tara.damavandi94@gmail.com>
2021-04-28 13:51:57 +0430

    Address file updated

--- src/Address.java
+++ src/Address.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Address{
-    public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);
         String street = sc.nextLine();
         String city = sc.nextLine();
         String state = sc.nextLine();
         int postalCode = sc.nextInt();
         String country = sc.nextLine();
-    }
+
     static void ValidateAsLabel(){
 
     }
