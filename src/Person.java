9a81fa72bc3a200d03cb21cddc0cddcaa586b3d6
tara <tara.damavandi94@gmail.com>
2021-04-28 13:53:29 +0430

    Person file updated

--- src/Person.java
+++ src/Person.java
@@ -2,15 +2,14 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
- public class Person{
-    public static void main(String[] args){
-        String name ;
-        int phoneNumber ;
-        String emailAddress;
-         ArrayList<Address> addresses ;
-            }
-    static void PurchaseParkingPass(){
+ public class Person {
 
-    }
+     String name;
+     int phoneNumber;
+     String emailAddress;
+     ArrayList<Address> addresses;
 
-}
+     static void PurchaseParkingPass() {
+
+     }
+ }
